5ca0ef44b88e4403dc19b87bf3eaa0703fc0acc2
“katherinesol” <katherine.sol@pm.me>
2026-03-14 02:46:36 -0400

    Build out class for retrieving remote dat

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -3,11 +3,16 @@ import json
 
 class GetRequester:
 
+    # Initialize with URL
     def __init__(self, url):
         self.url = url
 
+    # Send GET request and return raw bytes
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content  # Returns bytes (b"...")
 
+    # Send GET request and return parsed JSON as Python data
     def load_json(self):
-        pass
+        response = requests.get(self.url)
+        return response.json()  # Returns list/dict
